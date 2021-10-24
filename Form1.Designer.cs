568057b04e61880d8c3738af85d08dff7c1d0cee
Waysyy <74637067+Waysyy@users.noreply.github.com>
2021-10-24 18:11:16 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace minsquare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(916, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(317, 194);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "x";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "y";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(11, 12);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(898, 570);
+            this.zedGraphControl1.TabIndex = 1;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(916, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(317, 56);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(916, 301);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(317, 56);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Выбрать файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(916, 281);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Excel";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(916, 371);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "GoogleSheets";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(916, 419);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(319, 22);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(916, 399);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "api";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(916, 447);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(317, 56);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Использовать ссылку";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1245, 587);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+using Exc = Microsoft.Office.Interop.Excel;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace minsquare
+{
+    
+        public partial class Form1 : Form
+        {
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+            GraphPane pane = zedGraphControl1.GraphPane;
+            pane.Title.Text = "График";
+
+        }
+
+        public void Excel()
+        {
+            try
+            {
+                string str;
+                int rCnt;
+                int cCnt;
+
+                OpenFileDialog opf = new OpenFileDialog();
+                opf.Filter = "Файл Excel|*.XLSX;*.XLS";
+                opf.ShowDialog();
+                System.Data.DataTable tb = new System.Data.DataTable();
+                string filename = opf.FileName;
+
+                Exc.Application ExcelApp = new Exc.Application();
+                Exc._Workbook ExcelWorkBook;
+                Exc.Worksheet ExcelWorkSheet;
+                Exc.Range ExcelRange;
+
+                ExcelWorkBook = ExcelApp.Workbooks.Open(filename, 0, true, 5, "", "", true, Exc.XlPlatform.xlWindows, "\t", false,
+                    false, 0, true, 1, 0);
+                ExcelWorkSheet = (Exc.Worksheet)ExcelWorkBook.Worksheets.get_Item(1);
+
+                ExcelRange = ExcelWorkSheet.UsedRange;
+                for (rCnt = 1; rCnt <= ExcelRange.Rows.Count; rCnt++)
+                {
+                    dataGridView1.Rows.Add(1);
+                    for (cCnt = 1; cCnt <= 2; cCnt++)
+                    {
+                        str = (string)(ExcelRange.Cells[rCnt, cCnt] as Exc.Range).Text;
+                        dataGridView1.Rows[rCnt - 1].Cells[cCnt - 1].Value = str;
+                    }
+                }
+                ExcelWorkBook.Close(true, null, null);
+                ExcelApp.Quit();
+
+                releaseObject(ExcelWorkSheet);
+                releaseObject(ExcelWorkBook);
+                releaseObject(ExcelApp);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show($"Ошибочка вышла: \n {err.Message}");
+            }
+        }
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Невозможно очистить " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        
+        }
+        public void Datagrid()
+        {
+            try
+            {
+                GraphPane pane = zedGraphControl1.GraphPane;
+                pane.CurveList.Clear();
+                PointPairList list = new PointPairList();
+                PointPairList list1 = new PointPairList();
+                PointPairList list2 = new PointPairList();
+
+                string[] x;
+                string[] y;
+                
+                string yd = "";
+                string xd = "";
+                double n = 0;
+                
+                double sum1 = 0;
+                double sum2 = 0;
+                double sum3 = 0;
+                double sum4 = 0;
+
+                x = new string[dataGridView1.RowCount-1];
+                y = new string[dataGridView1.RowCount-1];
+                for (int i = 0; i < dataGridView1.RowCount-1; ++i)
+                {
+                    x[i] = dataGridView1[0, i].Value.ToString();
+                    y[i] = dataGridView1[1, i].Value.ToString();
+                    xd = x[i];
+                    yd = y[i];
+                    sum1 += Convert.ToDouble(xd);
+                    sum1 += Convert.ToDouble(yd);
+                    sum2 += Convert.ToDouble(xd);
+                    sum3 += Convert.ToDouble(yd);
+                    sum4 += Convert.ToDouble(xd) * Convert.ToDouble(xd);
+                    n = dataGridView1.Rows.Count;
+                    list.Add(Convert.ToDouble(xd), Convert.ToDouble(yd));
+
+                    
+                }
+               
+
+                double a = (sum1 - (sum2 * sum3)) / sum4 - (sum2 * sum2);
+                double b = (sum3 - (sum2 * a)) / n;
+                double h = (Math.Abs(b - a)) / 100;
+
+                for (double x1 = a; x1 <= b; x1 += h)
+                {
+                    list1.Add(x1, x1+b);
+
+                }
+
+                LineItem polinom = pane.AddCurve("Sinc", list1, Color.Blue, SymbolType.None);
+
+                for (double x1 = a; x1 <= b; x1 += h)
+                {
+                    list2.Add(x1, a*(x1*x1)+b*x1);
+
+                }
+                LineItem approx = pane.AddCurve("Sinc", list2, Color.Purple, SymbolType.None);
+
+                LineItem dot = pane.AddCurve("Sinc", list, Color.Red, SymbolType.Star);
+                dot.Line.IsVisible = false;
+
+                zedGraphControl1.AxisChange();
+                zedGraphControl1.Invalidate();
+
+  
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show($"Ошибочка вышла: \n {err.Message}");
+            }
+        }
+
+        public void GSheets()
+        {
+            try
+            {
+                string link;
+                WebRequest req = WebRequest.Create("https://docs.google.com/spreadsheets/d/" + @textBox1.Text + "/gviz/tq");
+                WebResponse res = req.GetResponse();
+                using (Stream stream = res.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        
+                        link = reader.ReadToEnd();
+
+                    }
+                }
+                link = link.Remove(0, 47);
+                var asd = link.LastIndexOf(")");
+                link = link.Remove(asd, 2);
+                res.Close();
+
+                //var result = JsonConvert.DeserializeObject<List<RootObject>>(link);
+
+                //var result = JsonConvert.DeserializeObject<SortedDictionary<string, C>>(link);
+
+                //C root = JsonConvert.DeserializeObject<C>(link);
+                //var result = root;
+                //MessageBox.Show(Convert.ToString(result));
+
+                 var result = JsonConvert.DeserializeObject<RootObject>(link);
+                dataGridView1.DataSource = result;
+
+                
+                
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show($"Ошибочка вышла: \n {err.Message}");
+            }
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Datagrid();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Excel();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            GSheets();
+        }
+    }
+}
